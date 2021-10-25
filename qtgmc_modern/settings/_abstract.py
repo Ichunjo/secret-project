0dd5687e593ed9221eaf1bdbb5450e37684f620d
Ichunjo <ichunjo.le.terrible@gmail.com>
2021-10-25 02:07:53 +0200

    minor changes on LoggedSettings

--- qtgmc_modern/settings/_abstract.py
+++ qtgmc_modern/settings/_abstract.py
@@ -15,22 +15,22 @@ class LoggedSettings(MutableMapping[str, Any], ABC):
         super().__init__()
 
     def __getitem__(self, k: str) -> Any:
-        return self._data[k]
+        return self._data.__getitem__(k)
 
     def __setitem__(self, k: str, v: Any) -> None:
-        self._data[k] = v
+        self._data.__setitem__(k, v)
         log_update((self.__class__.__name__, k), v)
 
     def __delitem__(self, v: str) -> NoReturn:
         raise NotImplementedError
 
     def __iter__(self) -> Iterator[str]:
-        yield from self._data
+        return self._data.__iter__()
 
     def __len__(self) -> int:
-        return len(self._data)
+        return self._data.__len__()
 
     def __str__(self) -> str:
-        return dict.__str__(self._data)
+        return self._data.__str__()
 
     __repr__ = __str__
